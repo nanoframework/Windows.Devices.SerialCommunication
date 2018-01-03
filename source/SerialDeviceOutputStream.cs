9e6a59dceac12a94b4eb5a705322ae72044c4815
José Simões <jose.simoes@eclo.solutions>
2018-01-03 12:28:31 +0000

    Work on SerialDeviceOutputStream and SerialDevice
    - Rework SerialDeviceOutputStream to use native calls from parent SerialDevice
    - Add new native calls to SerialDevice
    - Update CorLib Nuget package
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- source/SerialDeviceOutputStream.cs
+++ source/SerialDeviceOutputStream.cs
@@ -3,6 +3,7 @@
 // See LICENSE file in the project root for full license information.
 //
 
+using System;
 using Windows.Storage.Streams;
 
 namespace Windows.Devices.SerialCommunication
@@ -18,12 +19,22 @@ namespace Windows.Devices.SerialCommunication
 
         public bool Flush()
         {
-            throw new System.NotImplementedException();
+            // call 'store' to UART
+            _serialDevice.NativeStore();
+
+            return true;
         }
 
         public void Write(byte[] buffer)
         {
+            // write to UART stream
             _serialDevice.NativeWrite(buffer);
+        }
+
+        public uint Store()
+        {
+            // store to UART
+            return _serialDevice.NativeStore();
         }
     }
 }
