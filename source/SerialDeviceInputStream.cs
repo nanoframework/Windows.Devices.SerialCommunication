f6c102801465c4f8c751a26f37d26b2446aa28b6
José Simões <josesimoes@users.noreply.github.com>
2018-01-10 02:18:26 +0000

    Work on InputStream (#7)

---
+++ source/SerialDeviceInputStream.cs
@@ -0,0 +1,25 @@
+﻿//
+// Copyright (c) 2017 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using Windows.Storage.Streams;
+
+namespace Windows.Devices.SerialCommunication
+{
+    internal class SerialDeviceInputStream : IInputStream
+    {
+        private readonly SerialDevice _serialDevice;
+
+        public SerialDeviceInputStream(SerialDevice serialDevice)
+        {
+            _serialDevice = serialDevice;
+        }
+
+        public uint Read(byte[] buffer, uint count, InputStreamOptions options)
+        {
+            // write to UART stream
+            return _serialDevice.NativeRead(buffer, (int)options);
+        }
+    }
+}
