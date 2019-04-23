7762ebaaf3f628b163bc8a972e7f0689f93daa2a
José Simões <jose.simoes@eclo.solutions>
2019-04-23 13:31:40 +0100

    Add AssemblyNativeVersion

--- source/Windows.Devices.SerialCommunication/Properties/AssemblyInfo.cs
+++ source/Windows.Devices.SerialCommunication/Properties/AssemblyInfo.cs
@@ -10,6 +10,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Windows.Devices.SerialCommunications")]
 [assembly: AssemblyCopyright("Copyright © nanoFramework Contributors 2017")]
 
+////////////////////////////////////////////////////////////////
+// update this whenever the native assembly signature changes //
+[assembly: AssemblyNativeVersion("1.1.0.0")]
+////////////////////////////////////////////////////////////////
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
