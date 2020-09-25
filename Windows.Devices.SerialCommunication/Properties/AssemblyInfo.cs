3d5448d3344a0461bfc1d4b83a5526d79af79f60
josesimoes <jose.simoes@eclo.solutions>
2020-09-25 16:41:28 +0100

    Update copyright headers
    ***NO_CI***

--- Windows.Devices.SerialCommunication/Properties/AssemblyInfo.cs
+++ Windows.Devices.SerialCommunication/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Windows.Devices.SerialCommunications")]
 [assembly: AssemblyCompany("nanoFramework Contributors")]
 [assembly: AssemblyProduct("Windows.Devices.SerialCommunications")]
-[assembly: AssemblyCopyright("Copyright © nanoFramework Contributors 2017")]
+[assembly: AssemblyCopyright("Copyright (c) .NET Foundation and Contributors")]
 
 ////////////////////////////////////////////////////////////////
 // update this whenever the native assembly signature changes //
